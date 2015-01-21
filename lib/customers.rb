bd9798a47cdcd6dd5f148fc6218b262285004499
Teresa Finn <teresa.finn@gmail.com>
2015-01-20 20:25:31 -0700

    Added more tests and minimal class code to customers class and test.

--- lib/customers.rb
+++ lib/customers.rb
@@ -8,4 +8,17 @@ class Customers
 
   # load the csv
 
+  def all
+    #returns all instances of a class
+  end
+
+  def random 
+    #returns a random instance
+  end
+
+  def find_by_X(match)
+  end
+
+
+
 end
