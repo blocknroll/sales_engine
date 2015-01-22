43fcdc73b9a93a47de0bc54f2aafb566d74582fe
Teresa Finn <teresa.finn@gmail.com>
2015-01-22 13:20:20 -0700

    Added customer, customer repo, customer parser and tests.

--- test/customer_parser_test.rb
+++ test/customer_parser_test.rb
@@ -1,18 +1,35 @@
-require 'minitest/test'
-require 'minitest/pride'
-require 'csv'
+require 'minitest/autorun'
+# require 'minitest/pride'
+
 require_relative '../lib/customer_parser'
 
 class CustomerParserTest < MiniTest::Test
+# the customer parser takes in a csv file and turns the blob of data into customer objects. it must use the customer class in order to do this. it then passes the customers to the customer repo. 
 
   def setup
-    @customer = Customer.new(row)
-    @customers_csv = CSV.open('../test/support/customers_fixtures.csv', headers: true, header_converters: :symbol)
+    @customer_parser = CustomerParser.new
   end
 
   def test_it_exists
-    assert @customer
+    assert @customer_parser
   end
+
+
+
+  #build up to this test
+  def test_it_can_read_a_csv_file
+    last_row_name = "Ramona"
+    customer_file = "fixtures/customers_fixtures.csv"
+    assert_equal last_row_name, @customer_parser.make_customers(customer_file).last.first_name
+    #your parser needs a method to open the file  
+  end
+
+  def test_it_creates_a_customer_from_a_row
+    skip
+  end
+
+
+
 
   # def test_it_
   #
