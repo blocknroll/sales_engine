f2daf485618801a5f146358572f006f02c031016
Teresa Finn <teresa.finn@gmail.com>
2015-01-23 00:51:43 -0700

    Added transaction file and test file, completed.

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,12 +1,12 @@
 class Transaction
-  attr_reader :id, :invoice_id, :credit_card_number, :credit_card_expiration_date, :result
+  attr_reader :id, :invoice_id, :cc_number, :cc_expir_date, :result
 
-  def initialize(row)
-    @id = row[:id].to_i
-    @invoice_id = invoice_id
-    @credit_card_number = credit_card_number
-    @credit_card_expiration_date = credit_card_expiration_date
-    @result = result
+  def initialize(data)
+    @id = data[:id]
+    @invoice_id = data[:invoice_id]
+    @cc_number = data[:cc_number]
+    @cc_expir_date = data[:cc_expir_date]
+    @result = data[:result]
   end
 
 end
---
+++ test/transaction_test.rb
@@ -0,0 +1,40 @@
+require_relative 'test_helper'
+
+class TransactionTest < Minitest::Test
+
+  def setup
+    data = {
+      :id => 1,
+      :invoice_id => 1, 
+      :cc_number => 4654405418249632, 
+      :cc_expir_date => nil, 
+      :result => "success"
+    }
+    @transaction = Transaction.new(data)
+  end
+
+  def test_it_exists
+    assert @transaction
+  end
+
+  def test_it_has_an_id
+    assert_equal 1, @transaction.id
+  end
+
+  def test_it_has_an_invoice_id
+    assert_equal 1, @transaction.invoice_id
+  end
+
+  def test_it_has_a_cc_number
+    assert_equal 4654405418249632, @transaction.cc_number
+  end
+
+  def test_it_has_a_cc_expir_date
+    assert_equal nil, @transaction.cc_expir_date
+  end
+
+   def test_it_has_a_result
+    assert_equal "success", @transaction.result
+  end
+
+end
