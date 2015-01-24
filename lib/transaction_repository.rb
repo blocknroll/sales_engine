fae7aea86f760150c8e75f17d814141c5a2857a2
Teresa Finn <teresa.finn@gmail.com>
2015-01-24 11:58:35 -0700

    Completed transaction_repo class and test.

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -24,8 +24,29 @@ class TransactionRepository
     match.nil? ? "No match, try again." : match
   end
 
-  def find_by_cc_number
-    
+  def find_by_cc_number(cc_number)
+    match = all.detect { |e| e.cc_number == cc_number }
+    match.nil? ? "No matches, try again." : match
+  end
+
+  def find_by_result(result)
+    match = all.detect { |e| e.result == result }
+    match.nil? ? "No matches, try again." : match
+  end
+
+  def find_all_by_invoice_id(invoice_id)
+    matches = all.select { |e| e.invoice_id == invoice_id }
+    matches.nil? ? [] : matches
+  end
+
+  def find_all_by_cc_number(cc_number)
+    matches = all.select { |e| e.cc_number == cc_number }
+    matches.nil? ? "No matches, try again." : matches
+  end
+
+  def find_all_by_result(result)
+    matches = all.select { |e| e.result == result }
+    matches.nil? ? "No matches, try again." : matches
   end
 
 end
