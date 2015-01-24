04eae52f18276fa1416193b3f269bfcc74a3f9d9
Teresa Finn <teresa.finn@gmail.com>
2015-01-24 16:01:41 -0700

    Added invoice_item and test file, both complete.

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,8 +1,9 @@
 class InvoiceItem
+
   attr_reader :id, :item_id, :invoice_id, :quantity, :unit_price
 
-  def initialize(row)
-    @id = row[:id].to_i
+  def initialize(id, item_id, invoice_id, quantity, unit_price)
+    @id = id
     @item_id = item_id
     @invoice_id = invoice_id
     @quantity = quantity
@@ -10,3 +11,13 @@ class InvoiceItem
   end
 
 end
+
+  # attr_reader :id, :item_id, :invoice_id, :quantity, :unit_price
+
+  # def initialize(data)
+  #   @id = data[:id].to_i
+  #   @item_id = item_id
+  #   @invoice_id = invoice_id
+  #   @quantity = quantity
+  #   @unit_price = unit_price
+  # end
