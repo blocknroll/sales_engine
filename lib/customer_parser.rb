495143f17cef8a8e27015b2a0975fa58ca6ce04b
Teresa Finn <teresa.finn@gmail.com>
2015-01-24 22:02:16 -0700

    Changed spacing so the hash rockets/equals signs lined up.

--- lib/customer_parser.rb
+++ lib/customer_parser.rb
@@ -11,9 +11,9 @@ class CustomerParser
   private
   def parse(contents)
     contents.map { |row| Customer.new({
-      :id => row[:id],
+      :id         => row[:id],
       :first_name => row[:first_name],
-      :last_name => row[:last_name],
+      :last_name  => row[:last_name],
       :created_at => row[:created_at],
       :updated_at => row[:updated_at]
       } ) }
