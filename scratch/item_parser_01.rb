48f230158ccdaa615c1de01b7eabfe884a92f57d
Fred Block <info@fredblock.com>
2015-01-26 20:29:20 -0700

    Added item, item_parser and item_repsository to scratch folder.

---
+++ scratch/item_parser_01.rb
@@ -0,0 +1,22 @@
+require_relative 'item'
+
+class ItemParser
+
+  def make_items(item_file_to_read)
+    contents = CSV.open(item_file_to_read, headers: true, header_converters: :symbol)
+    parse_file(contents)
+  end
+
+  def parse_file(contents)
+    contents.map { |row| Item.new(
+      row[:id],
+      row[:name],
+      row[:description],
+      row[:unit_price],
+      row[:merchant_id],
+      row[:created_at],
+      row[:updated_at]
+      ) }
+    end
+
+  end
